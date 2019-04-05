0b57db9b12b6f8523e8bcb360b5d45f51f20ee46
Administrator <Administrator@FULLNAM-N2MCN28>
2019-04-05 18:31:54 +0900

    splite구조체 초기화(실수 방지)

--- pr2Dgame_01/SpliteObject.cpp
+++ pr2Dgame_01/SpliteObject.cpp
@@ -3,13 +3,19 @@
 SpliteObject::SpliteObject(std::string name, int tag, bool state, float px, float py)
 	:GameObject(name,tag,state,px,py)//게임오브젝트 내용을 상속받은 것
 {
+	//splite구조체 초기화
+	ZeroMemory(&splite,0);//splite.height = 0 등등 메모리를 모두 0으로 초기화
 }
 
 
 SpliteObject::~SpliteObject()
 {
 	//로드한 이미지 메모리를 해제함
-	delete splite.argb;
+	if (splite.argb != NULL)
+	{
+		delete[] splite.argb;
+		splite.argb = NULL;
+	}
 }
 
 void SpliteObject::draw()
