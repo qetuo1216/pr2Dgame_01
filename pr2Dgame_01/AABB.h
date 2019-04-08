b9bee123378a76856d001b34726bd49d5d631d0e
Administrator <Administrator@FULLNAM-N2MCN28>
2019-04-08 18:34:03 +0900

    AABB클래스 구현

---
+++ pr2Dgame_01/AABB.h
@@ -0,0 +1,14 @@
+#pragma once
+//회전되지 않는 사각형 바운딩박스(axis aligned bounding box) 클래스 구현
+class AABB
+{
+private:
+	//aabb좌표 크기
+	float x, y;//aabb왼쪽 상단 좌표
+	float width, height;//가로 세로 길이
+public:
+	//생성 소멸자
+	AABB();
+	virtual ~AABB();
+};
+
