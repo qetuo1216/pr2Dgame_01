dd59ac63540e26f570d91b891ad6ac1daaa09343
Administrator <Administrator@FULLNAM-N2MCN28>
2019-04-23 19:37:24 +0900

    바 길이 조정 테스트

--- pr2Dgame_01/HUDBar.cpp
+++ pr2Dgame_01/HUDBar.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "HUDBar.h"
-
+#include "graphic.h"
 
 HUDBar::HUDBar(float px, float py) : GameObject("HUDBar",2,true,px,py)
 {
@@ -15,13 +15,25 @@ HUDBar::~HUDBar()
 void HUDBar::init()
 {
 	//이미지 로드
-	readBmp("asset/Bar1Bk.bmp", &bar);
-	readBmp("asset/Bar1.bmp", &barBk);
+	readBmp("asset/Bar1.bmp", &bar);
+	readBmp("asset/Bar1Bk.bmp", &barBk);
+	//bar의 출력 길이값 테스트
+	value = bar.width / 2;
 }
 
 void HUDBar::draw()
 {
 	//bar 이미지 그리기
-	drawBmp(px, py, &bar);
+	//drawBmp(px, py, &bar);
+	for (int y = 0; y < bar.height; y++)
+	{
+		for (int x = 0; x < value; x++)
+		{
+			//x,y 위치에..픽셀을 출력함
+			unsigned int pixel = bar.argb[y*bar.width + x];
+			setPixel(px + x, py + y, pixel);
+		}
+	}
+	//bar 태두리 출력
 	drawBmp(px, py+50, &barBk);
 }
--- pr2Dgame_01/HUDBar.h
+++ pr2Dgame_01/HUDBar.h
@@ -6,6 +6,9 @@ class HUDBar : public GameObject
 private:
 	Sprite bar;//바 이미지
 	Sprite barBk;//바 태두리 이미지
+
+	//bar의 출력 길이
+	int value;
 public:
 	HUDBar(float px,float py);
 	~HUDBar();
