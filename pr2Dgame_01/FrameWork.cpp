d9b564ab6bf1a67e913f1d92571f52e3b6821497
Administrator <Administrator@FULLNAM-N2MCN28>
2019-04-24 19:04:25 +0900

    씬을 이동시키기 위해서 관리해 주기 위한 frameWork(sceneManager)를 만들어야한다. 여기서는 싱글톤을 사용한다. 프레임워크 = 게임 실행&종료+씬 전환

---
+++ pr2Dgame_01/FrameWork.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "FrameWork.h"
+
+
+FrameWork::FrameWork()
+{
+
+}
+
+
+FrameWork::~FrameWork()
+{
+}
---
+++ pr2Dgame_01/FrameWork.h
@@ -0,0 +1,10 @@
+#pragma once
+class FrameWork	//특별히 상속 필요 없다.
+{
+private:
+
+public:
+	FrameWork();
+	~FrameWork();
+};
+
