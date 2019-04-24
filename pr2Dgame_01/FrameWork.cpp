58094b34f8f469b3a1ab05f9de616cb4b2078a4e
Administrator <Administrator@FULLNAM-N2MCN28>
2019-04-24 19:12:34 +0900

    싱글톤으로 frameWork 만들기

--- pr2Dgame_01/FrameWork.cpp
+++ pr2Dgame_01/FrameWork.cpp
@@ -1,13 +1,36 @@
 #include "stdafx.h"
 #include "FrameWork.h"
 
+//static 포인터 만들기
+FrameWork * FrameWork::frameWork = NULL;
 
 FrameWork::FrameWork()
 {
 
 }
 
-
 FrameWork::~FrameWork()
+{
+}
+
+FrameWork * FrameWork::instance()
+{
+	if (frameWork == NULL)
+	{
+		frameWork = new FrameWork();//같은 클래스 안의 private는 접근 가능
+	}
+
+	return frameWork;
+}
+
+void FrameWork::init()
+{
+}
+
+void FrameWork::run()
+{
+}
+
+void FrameWork::exit()
 {
 }
--- pr2Dgame_01/FrameWork.h
+++ pr2Dgame_01/FrameWork.h
@@ -2,9 +2,19 @@
 class FrameWork	//특별히 상속 필요 없다.
 {
 private:
-
-public:
+	//생성자&소멸자
 	FrameWork();
-	~FrameWork();
+	virtual ~FrameWork();
+
+	//객체 저장하는 정적 포인터(객체 1개)
+	static FrameWork * frameWork;
+public:
+	//싱글톤 객체(인스턴스) 반환 메소드(static)
+	static FrameWork * instance();//pool이 null이면 객체를 만들고, 있으면 계속 사용한다.
+
+	//프레임워크 활용 메소드
+	void init();
+	void run();
+	void exit();
 };
 
